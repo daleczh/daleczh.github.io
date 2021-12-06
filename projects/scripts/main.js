16df80242519453166dc267db29101e69b078575
daleczh <chenzhaohua@pku.edu.cn>
2021-12-06 20:55:09 +0800

    update

---
+++ projects/scripts/main.js
@@ -0,0 +1,19 @@
+let heading = document.querySelector('h1');
+
+function setUserName() {
+    let name = prompt('Please tell me your name:');
+    if (!name || name === null) {
+        setUserName();
+    }
+    else {
+        // localStorage.setItem('name', name);
+        heading.textContent = heading.textContent + ' welcomes ' +  name;
+    }
+}
+
+let buttonYourName = document.querySelector('Button');
+buttonYourName.onclick = function () {
+    setUserName();
+}
+
+
